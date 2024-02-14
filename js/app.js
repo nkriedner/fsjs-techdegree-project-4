098a047f55940ce98bbc6ca66cbff687619e8e6b
Nils Kriedner <nkriedner@googlemail.com>
2024-02-14 15:17:53 +0100

    Add event listeners to start button & onscreen keyboard

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * app.js */
+// 'CLICK' event listener for 'Start Game' button
+document.querySelector("#btn__reset").addEventListener("click", () => {
+    console.log("click on start game button...");
+    // Hide overlay
+    document.querySelector("#overlay").style.display = "none";
+});
+
+// 'CLICK' event listener for each onscreen keyboard button
+const keyboardButtons = document.querySelectorAll(".key");
+for (let i = 0; i < keyboardButtons.length; i++) {
+    keyboardButtons[i].addEventListener("click", (e) => {
+        console.log("keyboard button clicked:", e.target.textContent);
+    });
+}
