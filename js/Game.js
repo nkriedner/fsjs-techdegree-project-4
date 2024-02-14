2e60a7c8d9cdbfecb11b2729bfc56a77584e01b1
Nils Kriedner <nkriedner@googlemail.com>
2024-02-14 15:06:17 +0100

    Create structure of Game class

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,33 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Game.js */
+// Create the Game Class
+class Game {
+    constructor() {
+        this.missed = 0;
+        this.phrases = [
+            "Life is beautiful.",
+            "We are all one.",
+            "Open your heart and see.",
+            "Just walk on.",
+            "This moment is all.",
+        ];
+        this.activePhrase = null;
+    }
+
+    startGame() {
+        console.log("startGame() running...");
+    }
+    getRandomPhrase() {
+        console.log("getRandomPhrase() running...");
+    }
+    handleinteraction() {
+        console.log("handleInteraction() running...");
+    }
+    removeLife() {
+        console.log("removeLife() running...");
+    }
+    checkForWin() {
+        console.log("checkForWin() running...");
+    }
+    gameOver() {
+        console.log("gameOver() running...");
+    }
+}
